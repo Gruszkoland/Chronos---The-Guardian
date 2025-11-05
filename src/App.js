9cd4577d20b2e7a1e348c0cc93105abd7c4f9198
Adrian <123732705+Gruszkoland@users.noreply.github.com>
2025-11-05 03:06:34 +0100

    Add src/App.js - Main React component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Chronos - The Guardian</h1>
+        <p>
+          Welcome to Chronos, your time management application.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
